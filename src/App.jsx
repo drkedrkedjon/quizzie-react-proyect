25ebc56151193bc0e18d5f0709d6d852360b4a86
Sasa Savic Perusina <sasa@bubulazy.com>
2023-01-08 21:26:16 +0000

    Doble carga de API resuelta

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,6 @@ import './App.css'
 
 function App() {
   const [start, setStart] = React.useState(false)
-  const [data, setData] = React.useState({})
-  
-  React.useEffect(function() {
-    fetch('https://opentdb.com/api.php?amount=5')
-    .then(res => res.json())
-    .then(preg => setData( oldData => oldData = {...preg} ))
-  }, [start])
-  
-  console.log(data)
 
   function StartQuizz() {
     setStart(true)
--- src/componentes/Quizz.jsx
+++ src/componentes/Quizz.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default function Quizz() {
+  const [data, setData] = React.useState({})
+  
+  React.useEffect(function() {
+    fetch('https://opentdb.com/api.php?amount=5')
+    .then(res => res.json())
+    .then(preg => setData( oldData => oldData = {...preg} ))
+  }, [])
+  
+  console.log(data)
 
   return (
     <h1>Quizz</h1>
