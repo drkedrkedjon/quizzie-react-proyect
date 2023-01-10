965204468ca606973676b1142dd302270749b510
Sasa Savic Perusina <sasa@bubulazy.com>
2023-01-10 13:30:09 +0000

    Decicion si las respuestas seran radios o bottones 2

--- src/componentes/Pregunta.jsx
+++ src/componentes/Pregunta.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function Pregunta(props) {
-  const radios = props.respuestas.map((res) => <p key={res}>{res}</p>);
-
+export default function Pregunta({ respuestas, pregunta, resCorrecta }) {
+  const radios = respuestas.map((res) => <p key={res}>{res}</p>);
   return (
     <div className="pregunta">
-      <h3>{props.pregunta}</h3>
+      <h3>{pregunta}</h3>
       <div className="pregunta--respuestas">{radios}</div>
     </div>
   );
--- src/componentes/Quizz.jsx
+++ src/componentes/Quizz.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Pregunta from "./Pregunta";
 
 export default function Quizz(props) {
+  // const [preguntasActuales, setPreguntasActuales] = React.useState({});
+  // console.log(preguntasActuales);
+
   const preguntas = props.data.results.map((pregunta) => {
     // Meter todas las posibles respuestas en un array
     const respuestas = [];
     respuestas.push(pregunta.correct_answer);
     pregunta.incorrect_answers.forEach((res) => respuestas.push(res));
 
-    // Hacer orden aleatoria para no tener la respuesta correcta en el mismo lugar.
+    // Hacer orden aleatoria del array resouestas para no tener la respuesta correcta en el mismo lugar.
     let indexActual = respuestas.length;
     let indexRandom = Math.floor(Math.random() * respuestas.length);
     while (indexActual !== 0) {
@@ -17,13 +20,13 @@ export default function Quizz(props) {
       respuestas[indexActual] = respuestas[indexRandom];
       respuestas[indexRandom] = temp;
     }
-    console.log(respuestas);
 
     return (
       <Pregunta
         key={pregunta.question}
         pregunta={pregunta.question}
         respuestas={respuestas}
+        resCorrecta={pregunta.correct_answer}
       />
     );
   });
