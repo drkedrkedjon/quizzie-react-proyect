81bb9467e5ded36427d1850d7f5a267fa518a801
Sasa Savic Perusina <sasa@bubulazy.com>
2023-01-10 14:43:35 +0000

    Desarollando radios funcionalidad y su state, trabado por momento

--- src/componentes/Pregunta.jsx
+++ src/componentes/Pregunta.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 
 export default function Pregunta({ respuestas, pregunta, resCorrecta }) {
-  const radios = respuestas.map((res) => <p key={res}>{res}</p>);
+  const [formData, setFormData] = React.useState({ respuesta: "" });
+
+  function manejarCambio(evento) {
+    const { name, value, type, checked } = evento.target;
+    setFormData((oldForm) => ({ ...oldForm, respuesta: checked }));
+  }
+
+  const radios = respuestas.map((res) => {
+    return (
+      <>
+        <input
+          key={res}
+          type="radio"
+          id={res}
+          name="respuestas"
+          value={res}
+          onChange={manejarCambio}
+          checked={formData.respuesta === { res }}
+        />
+        <label htmlFor={res}>{res}</label>
+      </>
+    );
+  });
+
   return (
     <div className="pregunta">
       <h3>{pregunta}</h3>
-      <div className="pregunta--respuestas">{radios}</div>
+      <form>
+        <fieldset className="pregunta--respuestas">{radios}</fieldset>
+      </form>
     </div>
   );
 }
+
+// <p key={res}>{res}</p>
